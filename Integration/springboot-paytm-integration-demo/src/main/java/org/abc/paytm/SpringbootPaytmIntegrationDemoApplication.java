5652262913dba4435772f909dfeb7146cc75ac5e
aadi <aadiraj48@gmail.com>
2021-01-28 07:50:22 +0530

    commting paytm integration code

---
+++ Integration/springboot-paytm-integration-demo/src/main/java/org/abc/paytm/SpringbootPaytmIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package org.abc.paytm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPaytmIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPaytmIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ Integration/springboot-paytm-integration-demo/src/test/java/org/abc/paytm/SpringbootPaytmIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abc.paytm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPaytmIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
